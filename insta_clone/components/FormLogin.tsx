0221423b1961e2d1f0512ac41789c606aef5942a
Pierre-Richard <mayer_alexandre@yahoo.fr>
2024-01-15 11:54:35 +0100

    create component FormLogin and FormRegister

--- insta_clone/components/FormLogin.tsx
+++ insta_clone/components/FormLogin.tsx
@@ -1,25 +1,59 @@
-import React, { ChangeEvent, useState } from "react";
+import React from "react";
+import axios from "axios";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-interface formProps {
+type UserSubmitForm = {
   email: string;
-  setEmail: React.Dispatch<React.SetStateAction<string>>;
   password: string;
-  setUserPassword: React.Dispatch<React.SetStateAction<string>>;
-  onSubmit?: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
-}
+};
 
-const FormLogin = ({
-  email,
-  setEmail,
-  password,
-  setUserPassword,
-  onSubmit,
-}: formProps) => {
+const FormLogin = () => {
+  const [, setCookies] = useCookies(["access_token"]);
+
+  const validationForm = Yup.object().shape({
+    email: Yup.string().required("Email is required").email("Email is invalid"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters")
+      .max(40, "Password must not exceed 40 characters"),
+  });
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<UserSubmitForm>({
+    resolver: yupResolver(validationForm),
+  });
+
+  const navigate = useNavigate();
+
+  const onSubmit: SubmitHandler<UserSubmitForm> = async (data) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/api/auth/login",
+        data
+      );
+
+      setCookies("access_token", response.data.token);
+      await window.localStorage.setItem("userId", response.data.userID);
+      await window.localStorage.setItem("access_token", response.data.token);
+      console.log(window.localStorage.getItem("access_token"));
+      navigate("/profil");
+    } catch (err) {
+      console.error("error: ", err);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center h-screen bg-indigo-600">
       <form
         className="w-96 p-6 shadow-lg bg-white rounded-md"
-        onSubmit={onSubmit}
+        onSubmit={handleSubmit(onSubmit)}
       >
         <h1 className="text-3xl block text-center font-semibold">Login</h1>
         <hr className="mt-3" />
@@ -29,16 +63,21 @@ const FormLogin = ({
             type="text"
             placeholder="Enter email"
             className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
+            {...register("email")}
           />
+          <div className="text-red-600 px-1 mt-1">{errors.email?.message}</div>
         </div>
         <div className="mt-3">
           <label className="block text-base mb-2">password</label>
           <input
             type="password"
             placeholder="Enter password"
-            value={password}
             className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
+            {...register("password")}
           />
+          <div className="text-red-600 px-1 mt-1">
+            {errors.password?.message}
+          </div>
         </div>
         <div className="mt-3 flex justify-between items-center">
           <a className="text-blue-500 font-semibold">Inscrivez-vous</a>
--- insta_clone/pages/Login.tsx
+++ insta_clone/pages/Login.tsx
@@ -1,44 +1,8 @@
-import React, { useState } from "react";
 import FormLogin from "../components/FormLogin";
-import axios from "axios";
-import { useCookies } from "react-cookie";
-import { useNavigate } from "react-router-dom";
 const Login = () => {
-  const [email, setEmail] = useState<string>("");
-  const [password, setUserPassword] = useState<string>("");
-
-  const [, setCookies] = useCookies(["access_token"]);
-  const navigate = useNavigate();
-
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(
-        "http://localhost:4000/api/auth/login",
-        {
-          email,
-          password,
-        }
-      );
-
-      setCookies("access_token", response.data.token);
-      await window.localStorage.setItem("userId", response.data.userID);
-      await window.localStorage.setItem("access_token", response.data.token);
-      console.log(window.localStorage.getItem("access_token"));
-      navigate("/profil");
-    } catch (err) {
-      console.error("error: ", err);
-    }
-  };
   return (
     <>
-      <FormLogin
-        email={email}
-        setEmail={setEmail}
-        password={password}
-        setUserPassword={setUserPassword}
-        onSubmit={onSubmit}
-      />
+      <FormLogin />
     </>
   );
 };

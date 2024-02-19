8b7e8ae456905b3379c961001ea396a554b1ad07
Pierre-Richard <mayer_alexandre@yahoo.fr>
2024-02-19 13:32:05 +0100

    Modified UI od page CreatePost

--- insta_clone/pages/CreatePost.tsx
+++ insta_clone/pages/CreatePost.tsx
@@ -4,8 +4,69 @@ const CreatePost = () => {
   return (
     <>
       <div className="flex justify-center items-center h-screen">
-        <input type="text" placeholder="ttile" />
-        <input type="text" placeholder="Body" />
+        <div className="w-full max-w-md">
+          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mt-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Title
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                placeholder="Enter title..."
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="body"
+              >
+                Body
+              </label>
+              <textarea
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="body"
+                placeholder="Enter body..."
+                rows="4"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="image"
+              >
+                Upload Image
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="image"
+                type="file"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="comments"
+              >
+                Comments
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="comments"
+                type="text"
+                placeholder="Enter comments..."
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-gradient-to-r from-blue-500 to-purple-500 hover:bg-gradient-to-r hover:from-blue-700 hover:to-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Create Post
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
